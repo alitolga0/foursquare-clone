d502c0431d6f09e5117413cce0715b77ec6e6e2d
alitolga0 <cakiralitolga@gmail.com>
2024-03-06 20:25:35 +0300

    Kodları yükledim

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# foursquare-clone
+# Foursquare Clone
+
+For Demo Page [Click Here](https://foursquare-clone.netlify.app/)
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            { test: /\.scss$/i, use: ["style-loader", "css-loader", "sass-loader"] },
+            { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+        ],
+    },
+    devServer: {
+        static: "./dist"
+    },
+
+};
